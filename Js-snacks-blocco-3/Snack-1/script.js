08d8ddf623bae03bb34742140c78f5788377dfb7
“Cal-107” <kaly___7@hotmail.it>
2021-11-08 18:48:07 +0100

    add final array with filter

--- Js-snacks-blocco-3/Snack-1/script.js
+++ Js-snacks-blocco-3/Snack-1/script.js
@@ -16,7 +16,7 @@
 
 // 1 - Creo un oggeto per un singolo giocatore con relative proprietà
 // - codice giocatore, percentuale punti per partita e successo tiri da 3 li definiamo tramite funzioni
-const playerCard = [
+const playerCard =
     {
         persId: setId() + getRandNumInt(100, 999),
         firstName: 'Michael',
@@ -24,13 +24,94 @@ const playerCard = [
         age: 28,
         pointForMatch: getRandNumInt(0, 50),
         percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
-    }
+    };
+
+// 3 - Stampo nome, cognome, età e codice giocatore.
+const {firstName, lastName, age, persId} = playerCard;
+
+// 4 - Creo un array di 10 giocatori
+const team10 = [
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Steph',
+        lastName: 'Curry',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Donovan',
+        lastName: 'Mitchell',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Derrick',
+        lastName: 'Rose',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Zion',
+        lastName: 'Williamson',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'LeBron',
+        lastName: 'James',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Alex',
+        lastName: 'Caruso',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Kobe',
+        lastName: 'Bryant',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Allen',
+        lastName: 'Iverson',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Kevin',
+        lastName: 'Durant',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
+    {
+        persId: setId() + getRandNumInt(100, 999),
+        firstName: 'Ja',
+        lastName: 'Morant',
+        pointForMatch: getRandNumInt(0, 50),
+        percSucc3Points: parseFloat(getRandNumPerc(0, 100).toFixed(1)),
+    },
 ];
-console.table(playerCard);
+console.log(team10);
 
+// 5 - Stampo nuovo array con statistiche migliori
 
-
-
+const team10filtered = team10.filter ( (team10) => {
+    if (team10.pointForMatch >= 35 && team10.percSucc3Points >= 80) {
+        return team10;
+    }
+});
+console.log(team10filtered);
 
 
 
